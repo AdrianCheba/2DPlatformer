5c88c460c06b8dcc37102a39775fa83e23a51f38
Beeriand <adiche@wp.pl>
2023-03-16 14:07:33 +0100

    Add MainMenu and LevelMenu

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public Canvas levelMenu;
+    public Canvas mainMenu;
+
+    public void btnLvl1()
+    {
+        levelMenu.enabled = false;
+        SceneManager.LoadScene("LvL1");
+    }
+
+    public void btnLvl2()
+    {
+        levelMenu.enabled = false;
+        SceneManager.LoadScene("LvL2");
+    }
+
+    public void btnLvl3()
+    {
+        levelMenu.enabled = false;
+        SceneManager.LoadScene("LvL3");
+    }
+    public void btnLvl4()
+    {
+        levelMenu.enabled = false;
+        SceneManager.LoadScene("LvL4");
+    }
+
+    public void btnLvl5()
+    {
+        levelMenu.enabled = false;
+        SceneManager.LoadScene("LvL5");
+    }
+    public void btnLvl6()
+    {
+        levelMenu.enabled = false;
+        SceneManager.LoadScene("LvL6");
+    }
+    public void btnLvl7()
+    {
+        levelMenu.enabled = false;
+        SceneManager.LoadScene("LvL7");
+    }
+    public void btnLvl8()
+    {
+        levelMenu.enabled = false;
+        SceneManager.LoadScene("LvL8");
+    }
+
+    public void btnBack()
+    {
+        mainMenu.enabled = true;
+        levelMenu.enabled = false;
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas mainMenu;
+    public Canvas levelMenu;
+
+    void Start()
+    {
+        mainMenu.enabled = true;    
+        levelMenu.enabled = false;
+    }
+
+    public void btnPlay()
+    {
+        mainMenu.enabled = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void btnLevels()
+    {
+        levelMenu.enabled = true;   
+        mainMenu.enabled = false;
+    }
+
+    public void btnExit()
+    {
+        Application.Quit();
+    }
+
+}
